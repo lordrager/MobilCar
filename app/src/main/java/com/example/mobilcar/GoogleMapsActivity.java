99a6d2424cc9be08da1cf261150486c64d3323b2
lordrager <lorenzoeccheli003@gmail.com>
2023-04-27 20:01:20 +0300

    Activity GoogleMapsActivity

---
+++ app/src/main/java/com/example/mobilcar/GoogleMapsActivity.java
@@ -0,0 +1,25 @@
+package com.example.mobilcar;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class GoogleMapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_googlemaps);
+        SupportMapFragment mapFragment = (SupportMapFragment)  getSupportFragmentManager().findFragmentById(R.id.maps);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+    }
+}
